55766bb9dc6857ef872e4b5cb9b013e928b265c1
Jonas Hiptmair <office@hiptmairit.at>
2021-02-20 16:16:30 +0100

    added subscription decorator export

--- src/active-connect.ts
+++ src/active-connect.ts
@@ -4,3 +4,4 @@ export { HttpServer } from "./server/http/server";
 export * from "./decorators/websocket/outbound";
 export * from "./decorators/websocket/route";
 export * from "./decorators/websocket/auth";
+export * from "./decorators/websocket/subscribe-changes";
