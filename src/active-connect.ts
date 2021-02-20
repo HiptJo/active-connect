bfbcfd71b6a209f9cb383cadd353e9ad8b1fe20c
Jonas Hiptmair <office@hiptmairit.at>
2021-02-20 16:16:59 +0100

    refactored filename

--- src/active-connect.ts
+++ src/active-connect.ts
@@ -4,4 +4,4 @@ export { HttpServer } from "./server/http/server";
 export * from "./decorators/websocket/outbound";
 export * from "./decorators/websocket/route";
 export * from "./decorators/websocket/auth";
-export * from "./decorators/websocket/subscribe-changes";
+export * from "./decorators/websocket/subscription";
