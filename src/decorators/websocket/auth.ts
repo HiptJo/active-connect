4b2c2f0fe81177c02a72e1102f53ef508f1158ef
Jonas Hiptmair <office@hiptmairit.at>
2021-02-20 16:59:49 +0100

    code cleanup

--- src/decorators/websocket/auth.ts
+++ src/decorators/websocket/auth.ts
@@ -4,25 +4,21 @@ import { WebsocketConnection } from "../../server/websocket/connection/connectio
 export function Auth(auth: WebsocketAuthenticator) {
   return function (target: any, propertyKey: string): any {
     // initialize routeDefinition
-    if (!propertyKey) {
-      throw Error("@Auth is not implemented for classes");
-    } else {
-      const original = target[propertyKey];
-      target[propertyKey] = async function (...data: any[]) {
-        let conn: WebsocketConnection;
-        if (data.length == 1) conn = data[0];
-        if (data.length > 1) conn = data[1];
+    const original = target[propertyKey];
+    target[propertyKey] = async function (...data: any[]) {
+      let conn: WebsocketConnection;
+      if (data.length == 1) conn = data[0];
+      if (data.length > 1) conn = data[1];
 
-        if (await auth.authenticate(conn)) {
-          return original(...data);
-        } else {
-          // check if it is a route (not a outlet)
-          if (!(target.___wsoutbound && target.___wsoutbound[propertyKey])) {
-            conn.send("m.error", "auth:unauthorized:" + auth.label);
-          }
-          return "error:auth:unauthorized";
+      if (await auth.authenticate(conn)) {
+        return original(...data);
+      } else {
+        // check if it is a route (not a outlet)
+        if (!(target.___wsoutbound && target.___wsoutbound[propertyKey])) {
+          conn.send("m.error", "auth:unauthorized:" + auth.label);
         }
-      };
-    }
+        return "error:auth:unauthorized";
+      }
+    };
   };
 }
