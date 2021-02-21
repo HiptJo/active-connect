a4300df56724d43a538ba5313f808ba688b74992
Jonas Hiptmair <office@hiptmairit.at>
2021-02-21 16:22:29 +0100

    #3 removed get/set override, as it causes variable overcuts and it is not required

--- src/decorators/websocket/shared.ts
+++ src/decorators/websocket/shared.ts
@@ -2,17 +2,5 @@ export function Shared<T>(defaultValue?: T) {
   return function (target: any, propertyKey: string) {
     if (!target.___data) target.___data = {};
     target.___data[propertyKey] = defaultValue;
-
-    const descriptor = {
-      get() {
-        return target.___data[propertyKey];
-      },
-      set(val: T) {
-        target.___data[propertyKey] = val;
-      },
-      enumerable: true,
-      configurable: true,
-    };
-    Object.defineProperty(target, propertyKey, descriptor);
   };
 }
