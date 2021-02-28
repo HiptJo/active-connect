eb957e0ac77cbf2c76791ca3059bad5af8cda0c8
Jonas Hiptmair <office@hiptmairit.at>
2021-02-28 10:12:19 +0100

    fixed route response handling

--- src/server/websocket/routing/route.ts
+++ src/server/websocket/routing/route.ts
@@ -48,9 +48,9 @@ export class WebsocketRoute {
     if (path.length === 1 && path[0] === this.method) {
       const res = await this.call(request);
       if (
-        res &&
-        !res.toString().startsWith("auth:unauthorized") &&
-        !res.toString().startsWith("error:auth:unauthorized")
+        !res ||
+        (!res.toString().startsWith("auth:unauthorized") &&
+          !res.toString().startsWith("error:auth:unauthorized"))
       ) {
         request.connection.send(`m.${request.path}`, res);
       }
