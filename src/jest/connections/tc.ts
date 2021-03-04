d586e9528180af338d4ca7b0230ec5650f03b216
Jonas Hiptmair <office@hiptmairit.at>
2021-03-04 18:10:32 +0100

    added tc to active-connect package

---
+++ src/jest/connections/tc.ts
@@ -0,0 +1,48 @@
+import { WebsocketClient } from "..";
+import { WebsocketConnection, WebsocketRequest } from "../../active-connect";
+import { WebsocketRouter } from "../../server/websocket/routing/router";
+
+export class TCWrapper extends WebsocketConnection {
+  public static router = new WebsocketRouter();
+  private client: WebsocketClient;
+  public constructor(token?: string | undefined) {
+    super(null);
+    this.token = token || null;
+    this.client = new WebsocketClient();
+
+    const _this = this;
+    this.client.defineSocketCallback(async function callback(
+      method: string,
+      data: any
+    ) {
+      return await TCWrapper.router.route(
+        new WebsocketRequest(method, data, _this)
+      );
+    });
+  }
+
+  private stack: {
+    method: string;
+    func: Function;
+  }[] = [];
+
+  send(method: string, value: any) {
+    if (this.stack.length > 0) {
+      if (this.stack[0].method == method) {
+        this.stack.shift()?.func(value);
+        if (method == "m.error") return;
+      }
+    }
+    if (method == "m.error") throw value;
+    this.client.messageReceived({ method, data: value });
+  }
+
+  async expectMethod(method: string): Promise<any> {
+    return new Promise((func) => {
+      this.stack.push({
+        method,
+        func,
+      });
+    });
+  }
+}
--- src/jest/index.ts
+++ src/jest/index.ts
@@ -1,5 +1,6 @@
 export * from "./angular-mocks/ng-router";
 export * from "./connections/testing-connection";
+export * from "./connections/tc";
 export * from "./angular-integration/client/client";
 export * from "./angular-integration/decorators/handle";
 export * from "./angular-integration/decorators/outbound";
