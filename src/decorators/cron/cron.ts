ae0565f2374650a1f4eae58374a203b25000d2ce
Jonas Hiptmair <office@hiptmairit.at>
2023-06-25 15:22:41 +0200

    improved cron decorator #69

--- src/decorators/cron/cron.ts
+++ src/decorators/cron/cron.ts
@@ -1,7 +1,18 @@
 const cron = require("node-cron");
 
+/**
+ * @decorator
+ * Creates a decorator @Cron("...") to create a cron task.
+ * @param crontab - The cron schedule in string format.
+ * @returns - The decorator function.
+ */
 export function Cron(crontab: string) {
-  return function _GET(target: any, propertyKey: string) {
+  /**
+   * Decorator function to bind a cron task to the target method.
+   * @param target - The target object.
+   * @param propertyKey - The property key of the target method.
+   */
+  return function _Cron(target: any, propertyKey: string) {
     cron.schedule(crontab, target[propertyKey].bind(target.___data));
   };
 }
