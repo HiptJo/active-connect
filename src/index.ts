dc1523cd9db37675c58ccb3e9ba5b1d817b6155f
Jonas Hiptmair <office@hiptmairit.at>
2023-07-10 17:04:23 +0200

    added load-current support #46

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,19 @@
 export * from "./server";
 export * from "./content";
 export * from "./decorators";
+
+export class ActiveConnect {
+  public static loadCurrentDirectory() {
+    var normalizedPath = require("path").join(__dirname);
+    require("fs")
+      .readdirSync(normalizedPath)
+      .forEach(function (file: string) {
+        if (
+          file.charAt(0) != "_" &&
+          (file.endsWith(".ts") || file.endsWith(".js")) &&
+          !file.endsWith(".spec.ts")
+        )
+          require("./" + file);
+      });
+  }
+}
