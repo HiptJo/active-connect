0b869f66501597a83d2b20684eec318c9b855554
Jonas Hiptmair <office@hiptmairit.at>
2023-07-16 16:26:16 +0200

    added docs #48

--- src/jest/cron/cron-tester.ts
+++ src/jest/cron/cron-tester.ts
@@ -1,10 +1,16 @@
 const cron = require("node-cron");
 
+/**
+ * A utility class for testing scheduled cronjobs using @Cron(...) annotation.
+ */
 export class CronTester {
   private static jobs: { crontab: string; func: Function }[] = [];
 
   private static initialized: boolean = false;
 
+  /**
+   * Initializes the CronTester by mocking the cron.schedule method.
+   */
   public static init() {
     CronTester.initialized = true;
     cron.schedule = jest.fn().mockImplementation((crontab, func) => {
@@ -12,6 +18,12 @@ export class CronTester {
     });
   }
 
+  /**
+   * Tests if a cron job with the specified crontab exists and executes the associated function.
+   * @param crontab - The crontab schedule string.
+   * @returns A promise that resolves with the value returned by the executed function.
+   * @throws If the CronTester has not been initialized.
+   */
   public static test(crontab: string): Promise<any> {
     if (!CronTester.initialized) {
       throw Error(
