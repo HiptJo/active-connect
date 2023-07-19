d6df69ae2fa37e3de460872f98926821a946b313
Jonas Hiptmair <office@hiptmairit.at>
2023-07-19 11:04:09 +0200

    refactored structure #74

--- src/index.ts
+++ src/index.ts
@@ -4,45 +4,4 @@ export * from "./content";
 export * from "./cron";
 export * from "./http";
 export * from "./websocket";
-
-export class ActiveConnect {
-  /**
-   * Loads all ts/js files in the speficied directory.
-   * Can be used to load route config without importing the files.
-   * @param dirname - absolute path of directory to load.
-   *
-   * @example
-   * ```
-   * ActiveConnect.loadCurrentDirectory(__dirname);
-   * ```
-   */
-  public static loadCurrentDirectory(dirname: string) {
-    var normalizedPath = require("path").join(dirname);
-    require("fs")
-      .readdirSync(normalizedPath)
-      .forEach(function (file: string) {
-        if (
-          file.charAt(0) != "_" &&
-          (file.endsWith(".ts") || file.endsWith(".js")) &&
-          !file.endsWith(".spec.ts")
-        )
-          require(dirname + "/" + file);
-      });
-  }
-
-  private static timeout = 3000;
-  /**
-   * When an expected method is not received in that amount of milliseconds, the test fails.
-   * @returns the timeout value
-   */
-  public static getTimeout() {
-    return ActiveConnect.timeout;
-  }
-  /**
-   * When an expected method is not received in that amount of milliseconds, the test fails.
-   * @returns the timeout value
-   */
-  public static setTimeout(ms: number) {
-    ActiveConnect.timeout = ms;
-  }
-}
+export * from "./active-connect";
